d69ed350fc8fac9d33a9ca72ac203c09aa832e77
Brian Sohn <kolpp15@gmail.com>
2022-09-29 14:05:48 -0700

    add button for new expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2022");
 
   const filterCHangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -16,19 +16,6 @@ const Expenses = (props) => {
   });
   // activate year filter
 
-  let expensesContent = <p>No Expenses Found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -37,7 +24,7 @@ const Expenses = (props) => {
           onChangeFilter={filterCHangeHandler}
         />
 
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
         {/* {filteredExpenses.length === 0 ? (
           <p>No Expenses Found.</p>
         ) : (
